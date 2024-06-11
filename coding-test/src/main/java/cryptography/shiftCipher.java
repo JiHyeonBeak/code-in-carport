9bdd539dc2ce94ac1693b883ba847f66f9de6980
JiHyeonBeak <jadehl09@gmail.com>
2024-06-11 09:16:02 +0900

    update decoding function

--- coding-test/src/main/java/cryptography/shiftCipher.java
+++ coding-test/src/main/java/cryptography/shiftCipher.java
@@ -44,7 +44,7 @@ public class shiftCipher {
                 shiftCipher(word);
                 break;
             case 2:
-                descramblingFunction(word);
+                decodingFunction(word);
                 break;
             default:
                 entryFunction();
@@ -54,8 +54,33 @@ public class shiftCipher {
     }
 
     // 복호화 함수
-    public void descramblingFunction(String decodingWord) {
+    public void decodingFunction(String decodingWord) {
+        // 복호화 편리를 위해, 입력받은 문자열을 대문자로 변경.
+        String word = decodingWord.toUpperCase();
+        // 아스키코드의 "A"값 설정.
+        int aNum = 65;
+        // 입력받은 문자열의 아스키코드를 byte배열에 넣는다.
+        byte[] bytes = word.getBytes(StandardCharsets.US_ASCII);
 
+        // 필요한 변수 설정.
+        String result = ""; // 결과 문자열
+        int getNum = 0;     // 암호화 중, 작업 할 문자의 아스키 코드 저장 변수.
+
+        for (int i=0; i <= bytes.length-1 ; i++) {
+            // 3자 뒤의 알파벳으로 치환한다.
+            getNum = bytes[i] - 3;
+
+            // 만약 A 이전의 문자라면, 다시 기준점을 Z로 초기화...
+            if(getNum < 65) {
+                int calNum = (65 - getNum)+aNum;
+                bytes[i] = (byte)calNum;
+            } else {
+                bytes[i] = (byte)getNum;
+            }
+            // 암호화가 완료되면 자료형을 바꿔 결과 문자열에 한 자 한 자 넣는다.
+            result += (char)bytes[i];
+        }
+        System.out.println(result);
     }
 
     // 암호화 함수
